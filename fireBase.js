bcb9833ac77a55dd6127f6f44a647be7731797b4
Anubhav Chachra <chachraanubhav@gmail.com>
2022-10-03 21:30:31 +0530

    firebase

--- fireBase.js
+++ fireBase.js
@@ -1,17 +1,15 @@
 var fireBase = fireBase || firebase;
 var hasInit = false;
 var config = {
-    apiKey: "AIzaSyCvhIPeRcsj_OCr6rUIs3OckE4zi3M2rYU",
-    authDomain: "yogaspire-60fc9.firebaseapp.com",
-    databaseURL: "https://yogaspire-60fc9.firebaseio.com",
-    projectId: "yogaspire-60fc9",
-    storageBucket: "yogaspire-60fc9.appspot.com",
-    messagingSenderId: "512885104040"
-  };
-if(!hasInit){
-    firebase.initializeApp(config);
-    hasInit = true;
+  apiKey: "AIzaSyAD0WRHdv59LTZS9VfIRs2H5AB3XN6kCHg",
+  authDomain: "yogai-cc884.firebaseapp.com",
+  projectId: "yogai-cc884",
+  storageBucket: "yogai-cc884.appspot.com",
+  messagingSenderId: "813527456400",
+  appId: "1:813527456400:web:2331953f7beb23955f5615",
+  measurementId: "G-QMEKS1L1M4",
+};
+if (!hasInit) {
+  firebase.initializeApp(config);
+  hasInit = true;
 }
-
-
-

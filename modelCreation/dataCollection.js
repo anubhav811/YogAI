ec285c6dcec39a2a6146a9fd63b7e063545104aa
Anubhav Chachra <chachraanubhav@gmail.com>
2022-10-07 11:02:45 +0530

    timer

--- modelCreation/dataCollection.js
+++ modelCreation/dataCollection.js
@@ -9,17 +9,17 @@ let targetPose;
 let state = "waiting";
 
 function keyPressed() {
- if (key == 's') {
+  if (key == "s") {
     yogi.saveData();
   } else {
     targetPose = key;
     console.log(targetPose);
-    setTimeout(function() {
-      console.log('collecting');
-      state = 'collecting';
-      setTimeout(function() {
-        console.log('not collecting');
-        state = 'waiting';
+    setTimeout(function () {
+      console.log("collecting");
+      state = "collecting";
+      setTimeout(function () {
+        console.log("not collecting");
+        state = "waiting";
       }, 5000);
     }, 5000);
   }
@@ -30,14 +30,14 @@ function setup() {
   video = createCapture(VIDEO);
   video.hide();
   poseNet = ml5.poseNet(video, modelLoaded);
-  poseNet.on('pose', gotPoses);
+  poseNet.on("pose", gotPoses);
 
   let options = {
     inputs: 34,
     outputs: 4,
     task: "classification",
-    debug: true
-  }
+    debug: true,
+  };
   yogi = ml5.neuralNetwork(options);
 }
 
@@ -45,7 +45,7 @@ function gotPoses(poses) {
   if (poses.length > 0) {
     pose = poses[0].pose;
     skeleton = poses[0].skeleton;
-    if (state == 'collecting') {
+    if (state == "collecting") {
       let inputs = [];
       for (let i = 0; i < pose.keypoints.length; i++) {
         let x = pose.keypoints[i].position.x;
@@ -55,17 +55,19 @@ function gotPoses(poses) {
       }
       let target = [targetPose];
       yogi.addData(inputs, target);
-}}}
+    }
+  }
+}
 
 function modelLoaded() {
-  console.log('poseNet ready');
+  console.log("openPose ready");
 }
 
 function draw() {
   translate(video.width, 0);
-  scale(-1,1);
+  scale(-1, 1);
   image(video, 0, 0, video.width, video.height);
-  
+
   if (pose) {
     for (let i = 0; i < skeleton.length; i++) {
       let a = skeleton[i][0];
@@ -75,5 +77,4 @@ function draw() {
       line(a.position.x, a.position.y, b.position.x, b.position.y);
     }
   }
-  
 }
